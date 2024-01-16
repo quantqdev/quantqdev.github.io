1cd7905903c077c1f284329ab6fa175f4620ae7f
Quan Ta <quantq.dev@gmail.com>
2024-01-16 18:03:41 +0700

    Update appearence

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,21 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-
 import sitemap from "@astrojs/sitemap";
+import react from "@astrojs/react";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://quantqdev.github.io",
-  integrations: [mdx(), sitemap()],
+  integrations: [
+    mdx(),
+    sitemap(),
+    react({
+      include: ["**/react/*"],
+    }),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
 });
